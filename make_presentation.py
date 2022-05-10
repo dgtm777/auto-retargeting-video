d45bda9a4a63883b9b08f3cbe502389eb22d6060
Daria <dgtm2001@gmail.com>
2022-05-11 00:45:45 +0700

    add options to make_presentation file

--- make_presentation.py
+++ make_presentation.py
@@ -3,18 +3,20 @@ import os
 import random
 from decoration import avs_subtitle
 from constants import all_videos, root_dir
+import constants
 from vapoursynth import core
 import vapoursynth as vs
 import ffmpeg
+from make_videos import make_videos
 
 
-core.std.LoadPlugin(
-    path="/home/linuxbrew/.linuxbrew/Cellar/ffms2/2.40_2/lib/libffms2.so"
-)
+# core.std.LoadPlugin(
+#     path="/home/linuxbrew/.linuxbrew/Cellar/ffms2/2.40_2/lib/libffms2.so"
+# )
 
-core.std.LoadPlugin(
-    path="/home/linuxbrew/.linuxbrew/Cellar/mvtools/23_1/lib/libmvtools.so"
-)
+# core.std.LoadPlugin(
+#     path="/home/linuxbrew/.linuxbrew/Cellar/mvtools/23_1/lib/libmvtools.so"
+# )
 
 
 def frame_to_numpy(frame):
@@ -28,14 +30,14 @@ def frame_to_numpy(frame):
 
 
 def make_presentation(
-    my_filename, premier_filename, out_filename_secret, out_filename_labeled
+    my_filename, premier_filename, out_filename_secret, out_filename_labeled, labeles
 ):
-    premiere_video = core.ffms2.Source(source=premier_filename)
+    comp_video = core.ffms2.Source(source=premier_filename)
     my_video = core.ffms2.Source(source=my_filename)
-    premiere_video = core.resize.Bicubic(
-        clip=premiere_video,
-        width=premiere_video.width,
-        height=premiere_video.height,
+    comp_video = core.resize.Bicubic(
+        clip=comp_video,
+        width=comp_video.width,
+        height=comp_video.height,
         format=vs.RGB24,
         matrix_in_s="709",
     )
@@ -50,13 +52,13 @@ def make_presentation(
     width = my_video.width
     if my_video.height <= my_video.width:
         func = np.vstack
-        height += premiere_video.height
+        height += comp_video.height
     else:
         func = np.hstack
-        width += premiere_video.width
-    premiere = (premiere_video, "premiere")
-    my = (my_video, "mine")
-    sequences = random.choice([[premiere, my], [my, premiere]])
+        width += comp_video.width
+    comp = (comp_video, labeles[1])
+    my = (my_video, labeles[0])
+    sequences = random.choice([[comp, my], [my, comp]])
     subtitled_seq = [
         avs_subtitle(sequences[0][0], sequences[0][1]),
         avs_subtitle(sequences[1][0], sequences[1][1]),
@@ -68,11 +70,9 @@ def make_presentation(
             pix_fmt="rgb24",
             s="{}x{}".format(width, height),
         )
+        .filter("setpts", f"N/({my_video.fps}*TB)+STARTPTS")
         .filter("fps", fps=my_video.fps, round="up")
-        .output(
-            out_filename_secret,
-            preset="ultrafast",
-        )
+        .output(out_filename_secret, preset="ultrafast", pix_fmt="yuv420p")
         .overwrite_output()
         .run_async(pipe_stdin=True)
     )
@@ -83,10 +83,12 @@ def make_presentation(
             pix_fmt="rgb24",
             s="{}x{}".format(width, height),
         )
+        .filter("setpts", f"N/({my_video.fps}*TB)+STARTPTS")
         .filter("fps", fps=my_video.fps, round="up")
         .output(
             out_filename_labeled,
             preset="ultrafast",
+            pix_fmt="yuv420p",
         )
         .overwrite_output()
         .run_async(pipe_stdin=True)
@@ -113,10 +115,263 @@ def make_presentation(
     process2.wait()
 
 
-for in_filename in all_videos:
+# for in_filename in all_videos:
+#     print(in_filename)
+#     make_videos(
+#         os.path.join(root_dir, "videos/" + in_filename + ".mp4"),
+#         os.path.join(
+#             root_dir,
+#             "videos/" + in_filename + "_cur.mp4",
+#         ),
+#     )
+#     make_presentation(
+#         os.path.join(root_dir, "videos/" + in_filename + "_cur.mp4"),
+#         os.path.join(root_dir, "videos/premiere/" + in_filename + ".mp4"),
+#         os.path.join(root_dir, "videos/presentation/premiere/" + in_filename + ".mp4"),
+#         os.path.join(
+#             root_dir, "videos/presentation/premiere/" + in_filename + "_labeled.mp4"
+#         ),
+#         ("mine", "premiere"),
+#     )
+
+videos = [
+    # "Sherbakov",
+    # "Svaty1",
+    # "Svaty2",
+    # "Svaty3",
+    # "trailer",
+    # "Masyanya",
+    # "TheBoysScene",
+    # "Fits",
+    # "Dogs",
+    # "HospitalTrick",
+    # "Domino",
+    # "NaskaFigurist",
+    # "NaskaFigurist2",
+    # "DenkaPastuh",
+    # "DenkaPastuh2",
+    # "Sobaki",
+    # "Richi",
+    # "Yulka",
+    # "Yulka2",
+    # "Denka1",
+]
+
+
+modes = [
+    # (
+    #     "weighted_sum/",
+    #     ("weighted sum", "no weighted sum"),
+    #     (
+    #         constants.future_speed_coef,
+    #         True,
+    #         (None,),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         constants.moving_available_coef,
+    #         constants.mask_coef,
+    #         constants.prev_speed_coef,
+    #         constants.jump_coef_img_size,
+    #         constants.jump_coef_mask_value,
+    #         False,
+    #     ),
+    # ),
+    # (
+    #     "mask_coef/",
+    #     ("mask coef", "no mask coef"),
+    #     (
+    #         constants.future_speed_coef,
+    #         True,
+    #         (None,),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         constants.moving_available_coef,
+    #         1,
+    #         constants.prev_speed_coef,
+    #         constants.jump_coef_img_size,
+    #         constants.jump_coef_mask_value,
+    #         True,
+    #     ),
+    # ),
+    # (
+    #     "wrong_scene/",
+    #     ("no wrong scene", "wrong scene"),
+    #     (
+    #         constants.future_speed_coef,
+    #         True,
+    #         (None,),
+    #         constants.fps_coef,
+    #         0,
+    #         constants.moving_available_coef,
+    #         constants.mask_coef,
+    #         constants.prev_speed_coef,
+    #         constants.jump_coef_img_size,
+    #         constants.jump_coef_mask_value,
+    #         True,
+    #     ),
+    # ),
+    # (
+    #     "shake/",
+    #     ("no shake", "shake"),
+    #     (
+    #         constants.future_speed_coef,
+    #         True,
+    #         (None,),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         0,
+    #         constants.mask_coef,
+    #         constants.prev_speed_coef,
+    #         constants.jump_coef_img_size,
+    #         constants.jump_coef_mask_value,
+    #         True,
+    #     ),
+    # ),
+    # (
+    #     "prev_speed/",
+    #     ("prev speed", "no prev speed"),
+    #     (
+    #         constants.future_speed_coef,
+    #         True,
+    #         (None,),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         constants.moving_available_coef,
+    #         constants.mask_coef,
+    #         0,
+    #         constants.jump_coef_img_size,
+    #         True,
+    #     ),
+    # ),
+    # (
+    #     "jump available/",
+    #     ("jump available", "no jump available"),
+    #     (
+    #         constants.future_speed_coef,
+    #         True,
+    #         (None,),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         constants.moving_available_coef,
+    #         constants.mask_coef,
+    #         constants.prev_speed_coef,
+    #         1,
+    #         True,
+    #     ),
+    # ),
+    # (
+    #     "future/",
+    #     ("future", "no future"),
+    #     (
+    #         0,
+    #         True,
+    #         (None,),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         constants.moving_available_coef,
+    #         constants.mask_coef,
+    #         constants.prev_speed_coef,
+    #         constants.jump_coef_img_size,
+    #         True,
+    #     ),
+    # ),
+    # (
+    #     "scene_detection/",
+    #     ("scene detection", "no scene detection"),
+    #     (
+    #         constants.future_speed_coef,
+    #         False,
+    #         (None,),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         constants.moving_available_coef,
+    #         constants.mask_coef,
+    #         constants.prev_speed_coef,
+    #         constants.jump_coef_img_size,
+    #         True,
+    #     ),
+    # ),
+    # (
+    #     "speed/",
+    #     ("adaptive speed", "constant speed"),
+    #     (
+    #         constants.future_speed_coef,
+    #         True,
+    #         (0, 0.002, 0.004, 0.006, 0.008, 0.01),
+    #         constants.fps_coef,
+    #         constants.jump_delay_coef,
+    #         constants.moving_available_coef,
+    #         constants.mask_coef,
+    #         constants.prev_speed_coef,
+    #         constants.jump_coef_img_size,
+    #         True,
+    #     ),
+    # ),
+]
+
+for place, labeles, flags in modes:
+    print(place)
+    for in_filename in videos:
+        print(in_filename)
+        for speed_flag in flags[2]:
+            label = ""
+            if speed_flag is not None:
+                label = f"_{speed_flag}"
+            make_videos(
+                os.path.join(root_dir, "videos/" + in_filename + ".mp4"),
+                os.path.join(
+                    root_dir,
+                    "videos/presentation/" + place + in_filename + label + ".mp4",
+                ),
+                future_speed_coef=flags[0],
+                scene_detection_flag=flags[1],
+                constant_speed=speed_flag,
+                fps_coef=flags[3],
+                jump_delay_coef=flags[4],
+                moving_available_coef=flags[5],
+                mask_coef=flags[6],
+                prev_speed_coef=flags[7],
+                jump_coef_img_size=flags[8],
+                weighted_sum=flags[9],
+            )
+        if len(flags[2]) == 1:
+            make_presentation(
+                os.path.join(root_dir, "videos/" + in_filename + "_cur.mp4"),
+                os.path.join(
+                    root_dir, "videos/presentation/" + place + in_filename + ".mp4"
+                ),
+                os.path.join(
+                    root_dir, "videos/presentation/" + place + in_filename + "_both.mp4"
+                ),
+                os.path.join(
+                    root_dir,
+                    "videos/presentation/" + place + in_filename + "_labeled.mp4",
+                ),
+                labeles,
+            )
+
+
+place = "speed/"
+names = [
+    # ("Denka1", "_0.01"),
+    # ("Dogs", "_0.01"),
+    # ("NaskaFigurist2", "_0.006"),
+    # ("Svaty1", "_0.002"),
+    # ("Svaty2", "_0.002"),
+]
+for in_filename, adds in names:
+    print("filename")
     make_presentation(
         os.path.join(root_dir, "videos/" + in_filename + "_cur.mp4"),
-        os.path.join(root_dir, "videos/premiere/" + in_filename + ".mp4"),
-        os.path.join(root_dir, "videos/presentation/" + in_filename + ".mp4"),
-        os.path.join(root_dir, "videos/presentation/" + in_filename + "_labeled.mp4"),
+        os.path.join(
+            root_dir, "videos/presentation/" + place + in_filename + adds + ".mp4"
+        ),
+        os.path.join(
+            root_dir, "videos/presentation/" + place + in_filename + "_both.mp4"
+        ),
+        os.path.join(
+            root_dir,
+            "videos/presentation/" + place + in_filename + "_labeled.mp4",
+        ),
+        ("adaptive speed", "constant speed = " + adds[1:]),
     )
