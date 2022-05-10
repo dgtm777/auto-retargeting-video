b24d01a45a5995108979ef2c45f0ef006efa4436
Daria <dgtm2001@gmail.com>
2022-05-11 00:33:05 +0700

    move parameters into constants.py file

--- constants.py
+++ constants.py
@@ -39,17 +39,35 @@ root_dir = os.path.dirname(__file__)
 all_videos = [
     "Fits",
     "Dogs",
-    "HospitalTrick",
     "Domino",
-    "Sherbakov",
-    "Masyanya",
+    "HospitalTrick",
     "NaskaFigurist",
     "NaskaFigurist2",
+    # "trailer",
     "DenkaPastuh",
     "DenkaPastuh2",
     "Sobaki",
-    "Sobaki2",
     "Richi",
-    "SevaVideo",
-    "trailer",
+    # "Sherbakov",
+    "Masyanya",
+    "TheBoysScene",
+    "Svaty1",
+    "Svaty2",
+    # "Svaty3",
+    # "Yulka",
+    # "Yulka2",
+    "Denka1",
 ]
+
+speed_error = float(0.001)
+mask_coef = int(4)
+fps_coef = float(1)
+speed_coef = int(6000)
+prev_speed_coef = float(0.9)
+future_speed_coef = float(0.5)
+jump_coef_img_size = float(0.25)
+jump_coef_wrap_size = float(2 / 3)
+jump_coef_mask_value = float(5)
+jump_delay_coef = float(0.8)
+moving_available_coef = float(1)
+scene_detection_parameters = (250, 150)
